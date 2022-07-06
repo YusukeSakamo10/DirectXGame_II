ec0d3a1f04c617fa0e56f73f3f199325f698398d
YusukeSakamoto <k021g1487@g.neec.ac.jp>
2022-07-06 13:09:42 +0900

    part3

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -7,145 +7,31 @@
 #define X_PI 3.1415f
 #define DEGREE_RADIAN(deg) (X_PI * (deg) / 180.0f)
 
-		GameScene::GameScene() {}
+GameScene::GameScene() {}
 
-	GameScene::~GameScene() {
-		delete model_;
-		delete debugCamera_;
-	}
+GameScene::~GameScene() {
+	delete model_;
+	delete debugCamera_;
+}
 
-	void GameScene::Initialize() {
+void GameScene::Initialize() {
 
-		dxCommon_ = DirectXCommon::GetInstance();
-		input_ = Input::GetInstance();
-		audio_ = Audio::GetInstance();
-		debugText_ = DebugText::GetInstance();
-
-
-		//画像の読み込み
-		textureHandle_ = TextureManager::Load("mario.jpg");
-
-		//3Dモデルの生成
-		model_ = Model::Create();
-
-		//ワールドトランスフォームの初期化
-		for (size_t i = 0; i < 3; i++) {
-			worldTransforms_[i].Initialize();
-		}
-		//ビュープロジェクションの初期化
-		viewProjection_.Initialize();
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
 
 
-		//デバッグカメラの生成
-		debugCamera_ = new DebugCamera(1280, 720);
-		//軸方向表示の表示を有効にする
-		AxisIndicator::GetInstance()->SetVisible(true);
-		//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
-		AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
-
-		//ライン描画が参照するビュープロジェクションを指定する
-		PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
-
-		//座標の設定
-		for (size_t i = 0; i < 3; i++) {
-
-			worldTransforms_[i].scale_ = { 1.0f, 1.0f, 1.0f };
-			switch (i) {
-			case 0:
-				worldTransforms_[i].translation_ = { 0.0f,-5.0f,10.0f };
-				break;
-			case 1:
-				worldTransforms_[i].translation_ = { -10.0f,5.0f,10.0f };
-				break;
-			case 2:
-				worldTransforms_[i].translation_ = { 10.0f,5.0f,10.0f };
-				break;
-			};
-
-			//変換行列を求める
-			worldTransforms_[i].matWorld_.WorldTransUpdate(worldTransforms_[i].scale_, worldTransforms_[i].rotation_, worldTransforms_[i].translation_);
-			worldTransforms_[i].TransferMatrix();
-		}
-	}
-
-	void GameScene::Update() {
-		//デバッグカメラの更新
-		debugCamera_->Update();
-		if (input_->TriggerKey(DIK_SPACE)) {
-			cameraNum_++;
-
-			if (cameraNum_ >= 3) {
-				cameraNum_ = 0;
-			}
-			moveTarget = worldTransforms_[cameraNum_].translation_ - viewProjection_.target;
-			moveTarget = moveTarget.normalize();
-		}
-		if (viewProjection_.target.x != worldTransforms_[cameraNum_].translation_.x) {
-			viewProjection_.target += moveTarget;
-		}
-		viewProjection_.UpdateMatrix();
-	}
-
-	void GameScene::Draw() {
-
-		// コマンドリストの取得
-		ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
-
-#pragma region 背景スプライト描画
-		// 背景スプライト描画前処理
-		Sprite::PreDraw(commandList);
-
-		/// <summary>
-		/// ここに背景スプライトの描画処理を追加できる
-		/// </summary>
-
-		// スプライト描画後処理
-		Sprite::PostDraw();
-		// 深度バッファクリア
-		dxCommon_->ClearDepthBuffer();
-#pragma endregion
-
-#pragma region 3Dオブジェクト描画
-		// 3Dオブジェクト描画前処理
-		Model::PreDraw(commandList);
-
-		/// <summary>
-		/// ここに3Dオブジェクトの描画処理を追加できる
-		/// </summary>
-
-		for (size_t i = 0; i < 3; i++) {
-			model_->Draw(worldTransforms_[i], viewProjection_, textureHandle_);
-		}
-
-		// 3Dオブジェクト描画後処理
-		Model::PostDraw();
-#pragma endregion
-
-#pragma region 前景スプライト描画
-		// 前景スプライト描画前処理
-		Sprite::PreDraw(commandList);
-
-		/// <summary>
-		/// ここに前景スプライトの描画処理を追加できる
-		/// </summary>
-
-		// デバッグテキストの描画
-		debugText_->DrawAll(commandList);
-		//
-		// スプライト描画後処理
-		Sprite::PostDraw();
-
-#pragma endregion
-	}
-
-
+	//画像の読み込み
+	textureHandle_ = TextureManager::Load("mario.jpg");
 
 	//3Dモデルの生成
 	model_ = Model::Create();
 
 	//ワールドトランスフォームの初期化
-	worldTransform_.Initialize();
-
+	for (size_t i = 0; i < 3; i++) {
+		worldTransforms_[i].Initialize();
+	}
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 
@@ -161,23 +47,43 @@
 	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
 
 	//座標の設定
-	worldTransform_.scale_ = { 5.0f, 5.0f, 5.0f };
-	float radian = DEGREE_RADIAN(45);
-	worldTransform_.rotation_ = {  radian,radian, 0.0f };
-	worldTransform_.translation_ = { 10.0f,10.0f,10.0f };
+	for (size_t i = 0; i < 3; i++) {
 
-	//変換行列を求める
-	worldTransform_.matWorld_.WorldTransUpdate(worldTransform_.scale_, worldTransform_.rotation_,worldTransform_.translation_);
+		worldTransforms_[i].scale_ = { 1.0f, 1.0f, 1.0f };
+		switch (i) {
+		case 0:
+			worldTransforms_[i].translation_ = { 0.0f,-5.0f,10.0f };
+			break;
+		case 1:
+			worldTransforms_[i].translation_ = { -10.0f,5.0f,10.0f };
+			break;
+		case 2:
+			worldTransforms_[i].translation_ = { 10.0f,5.0f,10.0f };
+			break;
+		};
 
-	
-	worldTransform_.TransferMatrix();
-
+		//変換行列を求める
+		worldTransforms_[i].matWorld_.WorldTransUpdate(worldTransforms_[i].scale_, worldTransforms_[i].rotation_, worldTransforms_[i].translation_);
+		worldTransforms_[i].TransferMatrix();
+	}
 }
 
 void GameScene::Update() {
 	//デバッグカメラの更新
 	debugCamera_->Update();
+	if (input_->TriggerKey(DIK_SPACE)) {
+		cameraNum_++;
 
+		if (cameraNum_ >= 3) {
+			cameraNum_ = 0;
+		}
+		moveTarget = worldTransforms_[cameraNum_].translation_ - viewProjection_.target;
+		moveTarget = moveTarget.normalize();
+	}
+	if (viewProjection_.target.x != worldTransforms_[cameraNum_].translation_.x) {
+		viewProjection_.target += moveTarget;
+	}
+	viewProjection_.UpdateMatrix();
 }
 
 void GameScene::Draw() {
@@ -207,10 +113,10 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 
+	for (size_t i = 0; i < 3; i++) {
+		model_->Draw(worldTransforms_[i], viewProjection_, textureHandle_);
+	}
 
-	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
-	
-	
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
